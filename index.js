ea730939e5b4ea01cd9d713e11f8ce2a2625926f
Kendall <kendall@kendalldoescoding.tech>
2023-05-12 14:34:52 +0530

    remove test quote

--- index.js
+++ index.js
@@ -78,11 +78,7 @@ var quotes = [
   {
     text: "You miss 100% of the shots you don't take.",
     author: "Wayne Gretzky"
-  },
-  {
-    text:"test",
-    author:"test"
-  },  
+  }, 
 ];
 // Function to generate a random quote
 function generateRandomQuote() {
